6db7f44d0969d717a2623464c234dbc3edec8764
Jeff Wainwright <yowainwright@gmail.com>
2017-11-30 00:31:52 -0800

    [update] remove deps

--- gulpfile.js
+++ gulpfile.js
@@ -4,16 +4,6 @@ const rename = require("gulp-rename");
 const sass = require('gulp-sass');
 const sassGlob = require('gulp-sass-glob');
 
-const yaml = require('gulp-yaml');
-const mustache = require('gulp-mustache-plus');
-const webserver = require('gulp-webserver');
-
-// gulp compile json from yml
-gulp.task('json', () => {
-  gulp.src('page/*.yml')
-  .pipe(yaml({ schema: 'DEFAULT_SAFE_SCHEMA'}))
-  .pipe(gulp.dest('page/'));
-});
 
 // COPY
 // copy in foot printless sass mixins
@@ -44,13 +34,4 @@ gulp.task('copy', () => {
     .pipe(gulp.dest('mixins/neat/'));
 });
 
-// Styles build styles for the landing page
-gulp.task('styles', () => {
-  return gulp
-  .src('page/main.scss')
-    .pipe(sassGlob())
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('page/'));
-});
-
-gulp.task('default', ['json', 'copy', 'styles']);
+gulp.task('default', ['copy']);
